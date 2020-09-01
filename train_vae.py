9b5fd09f858e6eb0b7e6ed54816cfff645c35084
unknown <ruili>
2020-09-02 03:33:11 +0800

    's'

--- train_vae.py
+++ train_vae.py
@@ -53,9 +53,9 @@ def run(dataset, data_dir, result_dir, config_id, num_gpus, total_kimg, gamma, m
     sched.G_lrate_base = sched.D_lrate_base = 0.002
     sched.G_lrate_dict = sched.D_lrate_dict = {128: 0.0015, 256: 0.002, 512: 0.003, 1024: 0.003}
     sched.minibatch_size_base = 32  # (default)
-    sched.minibatch_size_dict = {8: 256, 16: 128, 32: 64, 64: 1024*8}
+    sched.minibatch_size_dict = {8: 256, 16: 128, 32: 64, 64: 1024*4}
     sched.minibatch_gpu_base = 4  # (default)
-    sched.minibatch_gpu_dict = {8: 32, 16: 16, 32: 8, 64: 128*8}
+    sched.minibatch_gpu_dict = {8: 32, 16: 16, 32: 8, 64: 128*4}
 
     D_loss.gamma = 10
     metrics = [metric_defaults[x] for x in metrics]
