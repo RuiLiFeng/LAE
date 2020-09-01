81e3e5eefcf306b92ccb3ee8237fd7fb76f29a3f
unknown <ruili>
2020-09-02 03:22:29 +0800

    's'

--- training/loss.py
+++ training/loss.py
@@ -207,10 +207,10 @@ def vae_loss(G, D, opt, training_set, minibatch_size, reals, labels, pl_minibatc
     log_sigma = autosummary('Loss/scores/sigma', log_sigma)
     recon = autosummary('Loss/scores/recon', recon)
 
-    with tf.variable_scope('KL_divergence'):
+    with tf.name_scope('KL_divergence'):
         kld = autosummary('Loss/scores/kld', -tf.reduce_mean(tf.reduce_sum(
             0.5 * (1 + log_sigma - mu ** 2 - tf.exp(log_sigma)), 1)))
-    with tf.variable_scope('recon_loss'):
+    with tf.name_scope('recon_loss'):
         rl = autosummary('Loss/scores/rl', 2 * tf.reduce_mean(tf.reduce_sum(
             tf.square(reals - recon), [1, 2, 3])))
         # rl = -tf.reduce_mean(tf.reduce_sum(
