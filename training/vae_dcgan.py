42e25bd335ff923652f83ba1bd85a96ee69aaf15
unknown <ruili>
2020-09-03 18:42:52 +0800

    's'

--- training/vae_dcgan.py
+++ training/vae_dcgan.py
@@ -234,7 +234,7 @@ def Decoder_synthesis(
             z = tf.nn.relu(z)
 
     with tf.variable_scope('toRGB'):
-        z = conv2d_layer(z, fmaps=num_channels, kernel=1)
+        z = deconv2d(z, _out_shape, stddev=0.0099999, d_h=1, d_w=1, conv_filters_dim=5)
         images_out = tf.nn.tanh(z)
 
     return tf.identity(images_out, name='images_out')
