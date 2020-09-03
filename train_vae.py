b2547802d117e58f0f7a1d7bcbb242413fa7fc87
unknown <ruili>
2020-09-03 17:10:19 +0800

    's'

--- train_vae.py
+++ train_vae.py
@@ -51,9 +51,7 @@ def run(dataset, data_dir, result_dir, config_id, num_gpus, total_kimg, gamma, m
     train.total_kimg = total_kimg
     train.mirror_augment = mirror_augment
     train.image_snapshot_ticks = train.network_snapshot_ticks = 10
-    sched.G_lrate_base = sched.D_lrate_base = 0.002 * lr_mul
-    sched.G_lrate_dict = sched.D_lrate_dict = {128: 0.0015 * lr_mul, 256: 0.002 * lr_mul,
-                                               512: 0.003 * lr_mul, 1024: 0.003 * lr_mul}
+
     sched.batch_size = batch_size
     sched.lr = lr
     sched.decay_step = decay_step
